7022b370311ca33d9ad612243e8f7c92aef7f57a
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-07-06 15:45:55 +0900

    main
    main

--- src/app/shared/task-modal/new-item/new-item.component.ts
+++ src/app/shared/task-modal/new-item/new-item.component.ts
@@ -3,6 +3,13 @@ import { DataService } from '../../data-service/data.service';
 import { Subject, BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { MessageService } from 'primeng/api';
 import { ViewEncapsulation } from '@angular/core';
+import { OktaConfigService } from "../../okta/okta-config.service";
+import { OktaApiService } from "../../okta/okta-api.service";
+import { OktaGetTokenService } from '../../okta/okta-get-token.service';
+
+interface myTaskCat {
+  toDoCategories: string,
+}
 
 @Component({
   selector: 'app-new-item',
@@ -15,12 +22,108 @@ export class NewItemComponent implements OnInit {
   constructor(
     private DataService: DataService,
     private messageService: MessageService,
+    private OktaApiService: OktaApiService,
+    public OktaConfigService: OktaConfigService,
+    private OktaGetTokenService: OktaGetTokenService,
   ) { }
 
-  strItem
-  async ngOnInit(){
-    this.DataService.currentMessage.subscribe(message => (this.selectedMessage = message));
-    this.strItem=JSON.stringify(this.selectedMessage)
+  strItem;
+  strItemSubject;
+  strItemDesc;
+
+  myKey;
+  myAccessToken;
+  myEmail;
+  arrDownloadedTaskCat;
+  addTaskSelectedCat: myTaskCat[];
+  selectedCategory;
+
+  async ngOnInit() {
+    await this.DataService.currentMessage.subscribe(message => (this.selectedMessage = message));
+    this.myAccessToken = await this.OktaGetTokenService.GetAccessToken();
+    this.myKey = await this.myAccessToken.claims.myKey;
+    this.myEmail = await this.myAccessToken.claims.sub;
+
+    this.arrDownloadedTaskCat = await this.GetTaskCat(this.OktaConfigService.strMyToDoCatDownload, this.myKey, this.myEmail);
+
+    console.log(this.arrDownloadedTaskCat)
+    
+    this.strItem = await this.selectedMessage;
+    this.strItemSubject = await this.strItem.subject;
+    this.strItemDesc = await this.strItem.description;
   }
+
+  itemUpdateRes
+  async updateTask() {
+    this.myAccessToken = await this.OktaGetTokenService.GetAccessToken();
+    this.myKey = await this.myAccessToken.claims.myKey;
+    this.myEmail = await this.myAccessToken.claims.sub;
+
+    this.itemUpdateRes = await this.taskUpdate(this.OktaConfigService.strUpdateTaskContentURL, this.myKey, this.myEmail, this.strItem["Row ID"], this.strItem.subject, this.strItem.description, this.strItem.category),
+      console.log(this.itemUpdateRes)
+
+    switch (this.itemUpdateRes.status){
+      case "Task Updated":{
+        this.toastMsg="Task updated"
+        this.showSuccess()
+        break;
+      }
+      default:{
+        this.toastMsg="Error updating task"
+        this.showError()
+        break;
+      }
+    }
+    await window.location.replace('/todo')
+  }
+
+
+  async taskUpdate(url, mykey, email, rowId, sub, description, cat) {
+    let requestURI;
+    requestURI = url;
+
+    let requestBody;
+    requestBody = {
+      mykey: mykey,
+      email: email,
+      rowId: rowId,
+      sub: sub,
+      description: description,
+      cat: cat,
+
+    }
+
+    let taskUpdateRes;
+    taskUpdateRes = await this.OktaApiService.InvokeFlow(requestURI, requestBody);
+    // console.log(newWebApp.status);
+    return taskUpdateRes;
+  }
+
+  async GetTaskCat(url, mykey, email) {
+    let requestURI;
+    requestURI = url;
+
+    let requestBody;
+    requestBody = {
+      mykey: mykey,
+      email: email,
+    }
+    let toDoCat;
+    toDoCat = await this.OktaApiService.InvokeFlow(requestURI, requestBody);
+    return toDoCat;
+  }
+
+  toastMsg;
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: this.toastMsg });
+  }
+
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: this.toastMsg });
+  }
+  onReject() {
+    this.messageService.clear('c');
+  }
+
 
 }
